47b8cb670b9e97cea26e6969ce1deab96f259ad0
tomihm <tomas.hernandez@duocuc.cl>
2026-09-04 15:32:47 -0400

    Agregue clase Mago y guerrero con sus hechizos

--- src/main/kotlin/model/Guerrero.kt
+++ src/main/kotlin/model/Guerrero.kt
@@ -1,2 +1,13 @@
-package org.example.model
+package model;
+
+import kotlinx.coroutines.delay;
+
+class Guerrero(nombre: String, ataque: Int, vida: Int): Personaje(nombre, ataque, vida){
+    suspend fun golpeJusticiero(objetivo: Personaje){
+        println("$nombre lanza Golpe Justiciero")
+        delay (1000)
+        objetivo.recibirDano(ataque)
+
+    }
+}
 
--- src/main/kotlin/model/Mago.kt
+++ src/main/kotlin/model/Mago.kt
@@ -1,2 +1,17 @@
-package org.example.model
+package model;
+
+import kotlinx.coroutines.delay;
+
+class Mago (nombre: String, ataque: Int, vida: Int, val magia: Int) : Personaje(nombre, ataque, vida) {
+    suspend fun relampago (objetivo: Personaje){
+        println("$nombre lanza Relampago a {$objetivo.nombre}")
+        delay(3000)
+        objetivo.recibirDano(magia)
+    }
+    suspend fun ventisca (objetivo: Personaje){
+        println("$nombre lanza Ventisca a {$objetivo.nombre}")
+        delay(5000)
+        objetivo.recibirDano(magia*2)
+    }
+}
 
